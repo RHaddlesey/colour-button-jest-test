01a7573439c44b0c19f2d0e198c7b674b975c08c
Dr Richard Haddlesey <richard.haddlesey@sky.com>
2022-09-26 12:45:14 +0100

    disabled test

--- src/App.js
+++ src/App.js
@@ -2,17 +2,27 @@ import { useState } from 'react';
 
 function App() {
   const [buttonColour, setButtonColour] = useState('red');
+  const [disabled, setDisabled] = useState(false);
+
   const newButtonColour = buttonColour === 'red' ? 'blue' : 'red';
 
   return (
     <div className='App'>
       <button
-        style={{ backgroundColor: buttonColour }}
+        style={{ backgroundColor: buttonColour, color: 'white' }}
         onClick={() => setButtonColour(newButtonColour)}
+        disabled={disabled}
       >
         Change to {newButtonColour}
       </button>
-      <input type='checkbox' />
+      <br />
+      <input
+        type='checkbox'
+        id='enable-button-checkbox'
+        defaultChecked={disabled}
+        aria-checked={disabled}
+        onChange={(e) => setDisabled(e.target.checked)}
+      />
     </div>
   );
 }
